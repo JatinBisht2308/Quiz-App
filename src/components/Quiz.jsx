74f2c43607b62c67e8b1d39de6cd879ca2f4a34a
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-09-24 01:04:09 +0530

    :fire: completed the logic behind getting questions from api and also created simple prototype how data will be send as prototype

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
-const Quiz = () => {
+
+const Quiz = ({ques,wrongAnswer,rightAnswer}) => {
   return (
-    <div>
-      Quiz hu ma.
+    <div className="container mt-5 p-3 d-flex justify-content-center  align-center flex-column"
+    style={{backgroundColor:'#e5e1ff'}}
+    >
+     <h3>{ques}</h3>
+     <p>{rightAnswer}</p>
+     <ul>
+     {wrongAnswer.map((item) =>(
+      <li>{item}</li>
+     ))}
+     </ul>
     </div>
   );
 };
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import Navbar from "../components/Navbar";
 import Instructions from "../components/Instructions";
 import Quiz from "../components/Quiz";
@@ -12,6 +13,9 @@ const StartPage = () => {
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
   const [answeredQuestions, setAnsweredQuestions] = useState(0);
+  const [quizData, setQuizData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   useEffect(() => {
     const name = localStorage.getItem("name");
@@ -26,6 +30,18 @@ const StartPage = () => {
   }, []);
 
   const handleStartQuiz = () => {
+    axios.get("https://opentdb.com/api.php?amount=15").then((response) => {
+      // Check if the request was successful
+      console.log(response.status);
+      if (response.status === 200) {
+        // Set the fetched data to the quizData state
+        setQuizData(response.data.results);
+        console.log(response.data.results);
+        setIsLoading(false);
+      } else {
+        console.error("Failed to fetch data from the API");
+      }
+    });
     setShowInstructions(false);
     setShowQuiz(true);
   };
@@ -37,8 +53,9 @@ const StartPage = () => {
         className="row m-0 mainComp"
         style={{ height: `calc(100vh - ${navbarHeight}px)` }}
       >
-        <div className="col-md-3 left-section"
-        style={{backgroundColor: '#31393C'}}
+        <div
+          className="col-md-3 left-section"
+          style={{ backgroundColor: "#31393C" }}
         >
           <QuestionCounter
             totalQuestions={15}
@@ -56,7 +73,13 @@ const StartPage = () => {
               onStartQuiz={handleStartQuiz}
             />
           )}
-          {showQuiz && <Quiz />}
+          {!isLoading && showQuiz && (
+            <Quiz
+              ques={quizData[0].question}
+              rightAnswer={quizData[0].correct_answer}
+              wrongAnswer={quizData[0].incorrect_answers}
+            />
+          )}
         </div>
       </div>
     </div>
