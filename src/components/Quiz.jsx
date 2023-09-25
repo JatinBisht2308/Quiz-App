c71c4caba9c06d86b688e154a78b1f7c80ece7b0
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-09-25 16:09:31 +0530

    :fire: fixed the problem behind radio checking and managing options state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 const Quiz = ({
   ques,
@@ -8,16 +9,16 @@ const Quiz = ({
   handlePreviousQuestion,
 }) => {
   const [options, setOptions] = useState([]);
-
+  const [selectedOption, setSelectedOption] = useState(null);
   useEffect(() => {
     settingOptionsRandomly();
+    setSelectedOption(null);
   }, [ques]); // Trigger when 'ques' prop changes
 
   const settingOptionsRandomly = () => {
     const optionsArray = wrongAnswer.slice();
     optionsArray.push(rightAnswer);
 
-    // Fisher-Yates Shuffle Algorithm
     for (let i = optionsArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [optionsArray[i], optionsArray[j]] = [optionsArray[j], optionsArray[i]];
@@ -25,25 +26,37 @@ const Quiz = ({
 
     setOptions(optionsArray);
   };
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
   const formattedQuestion = ques
     .replace(/&#039;/g, "'")
     .replace(/&quot;/g, '"');
   return (
-    <div
-      className="container mt-5 p-3 d-flex justify-content-center  align-center flex-column"
-      style={{ backgroundColor: "#e5e1ff" }}
-    >
-      <h3 className="mb-3">{formattedQuestion}</h3>
-      <ul>
+    <div className="container mt-5 p-3 d-flex justify-content-center  align-center flex-column">
+      <hr />
+      <h5 className="mb-3">{formattedQuestion}</h5>
+      <Form>
         {options.map((item) => (
-          <li>{item}</li>
+          <Form.Check
+            type="radio"
+            label={`${item}`}
+            checked={selectedOption === item}
+            onChange={() => handleOptionChange(item)}
+          />
         ))}
-      </ul>
+      </Form>
+      <hr />
       <div
-        className="d-flex justify-content-between mt-2"
+        className="d-flex justify-content-between mt-1"
         style={{ width: "13.8%" }}
       >
-        <button type="button" className="quiz-btn" onClick={handlePreviousQuestion}>
+        <button
+          type="button"
+          className="quiz-btn"
+          onClick={handlePreviousQuestion}
+        >
           <AiOutlineArrowLeft />
           &nbsp;Back
         </button>
